36e13cbcc7b15ffd732ec35526d9db3d5ff4f8d9
Ocean <dvsocean@icloud.com>
2018-09-23 14:50:18 -0700

    front page in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,188 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>VasquezSales</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
 
-        <title>Laravel</title>
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+				<!-- Header -->
+					<header id="header">
+						<a href="welcome.blade.php" class="logo">VasquezSales <span>by TheTruckGuys</span></a>
+						<nav>
+							<ul>
+								<li><a href="#menu">Menu</a></li>
+							</ul>
+						</nav>
+					</header>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+				<!-- Menu -->
+					<nav id="menu">
+						<ul class="links">
+							<li><a href="welcome.blade.php">Home</a></li>
+							<li><a href="generic.html">Generic</a></li>
+							<li><a href="elements.html">Elements</a></li>
+						</ul>
+						<ul class="actions vertical">
+							<li><a href="#" class="button special fit">Sign Up</a></li>
+							<li><a href="#" class="button fit">Log In</a></li>
+						</ul>
+					</nav>
 
-            .full-height {
-                height: 100vh;
-            }
+				<!-- Banner -->
+				<!--
+					Note: If you'd like to use a video as your banner background, set the "data-video" attribute below
+					to the full filename of your video, but *exclude* the file extension. The template will automatically
+					generate a multi-format* <video> tag on platforms that support background videos, and simply skip the
+					step on those that don't (falling back on whatever you've set as the background image).
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+					* Your video must be offered in both .mp4 and .webm formats to work everywhere.
+				-->
+				{{--<section id="banner" data-video="images/banner">--}}
+					<section id="banner" data-video="">
+						<div class="inner">
+							<header>
+								<h1>Vasquez Sales</h1>
+								<p>Freightliner, Kenworth, Mack, Peterbuilt, Volvo, Hino, International</p>
+							</header>
+							<ul class="actions">
+								<li><a href="#" class="button special big">Get Started</a></li>
+							</ul>
+						</div>
+						<a href="#one" class="more">Learn More</a>
+					</section>
 
-            .position-ref {
-                position: relative;
-            }
+				<!-- Wrapper -->
+					<div id="wrapper">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+						<!-- One -->
+							<section id="one" class="main">
+								<div class="inner spotlight style1">
+									<div class="content">
+										<header>
+											<h2>Sed Consequat</h2>
+										</header>
+										<p>Ipsum efficitur nisi ac turpis venenatis varius. Suspendisse sed dictum leo ipsum amet pellentesque fusce ac hendrerit consectetur tempor risus, sed eget semper nunc. eleifend facilisis nisi vitae lorem ipsum dolor consequat lorem ipsum etiam.</p>
+									</div>
+									<!--
+										Note: You can replace the image below with a JPEG or PNG. Just make sure it's exactly
+										320x340 or at least the same aspect ratio (16:17).
+									-->
+									<span class="image"><img src="images/pic01.svg" alt="" /></span>
+								</div>
+							</section>
 
-            .content {
-                text-align: center;
-            }
+						<!-- Two -->
+							<section id="two" class="main">
+								<div class="inner spotlight alt style2">
+									<div class="content">
+										<header>
+											<h2>Nisl Adipiscing</h2>
+										</header>
+										<p>Ipsum efficitur nisi ac turpis venenatis varius. Suspendisse sed dictum leo ipsum amet pellentesque fusce ac hendrerit consectetur tempor risus, sed eget semper nunc. eleifend facilisis nisi vitae lorem ipsum dolor consequat lorem ipsum etiam.</p>
+									</div>
+									<!--
+										Note: You can replace the image below with a JPEG or PNG. Just make sure it's exactly
+										320x340 or at least the same aspect ratio (16:17).
+									-->
+									<span class="image"><img src="images/pic02.svg" alt="" /></span>
+								</div>
+							</section>
 
-            .title {
-                font-size: 84px;
-            }
+						<!-- Three -->
+							<section id="three" class="main">
+								<div class="inner spotlight style3">
+									<div class="content">
+										<header>
+											<h2>Varius Tempus</h2>
+										</header>
+										<p>Ipsum efficitur nisi ac turpis venenatis varius. Suspendisse sed dictum leo ipsum amet pellentesque fusce ac hendrerit consectetur tempor risus, sed eget semper nunc. eleifend facilisis nisi vitae lorem ipsum dolor consequat lorem ipsum etiam.</p>
+									</div>
+									<!--
+										Note: You can replace the image below with a JPEG or PNG. Just make sure it's exactly
+										320x340 or at least the same aspect ratio (16:17).
+									-->
+									<span class="image"><img src="images/pic03.svg" alt="" /></span>
+								</div>
+							</section>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+						<!-- Four -->
+							<section id="four" class="main special">
+								<div class="inner">
+									<header>
+										<h2>Tempor Suspendisse</h2>
+									</header>
+									<div class="features">
+										<section>
+											<span class="icon major fa-diamond style1"></span>
+											<h3>Magna Veroeros</h3>
+											<p>Consequat turpis venenatis varius lorem sed dictum ipsum amet pellentesque fusce hendrerit consectetur risus, sed eget semper nunc.</p>
+										</section>
+										<section>
+											<span class="icon major fa-laptop style2"></span>
+											<h3>Risus Adipiscing</h3>
+											<p>Consequat turpis venenatis varius lorem sed dictum ipsum amet pellentesque fusce hendrerit consectetur risus, sed eget semper nunc.</p>
+										</section>
+										<section>
+											<span class="icon major fa-bar-chart style3"></span>
+											<h3>Leo Ipsum Varius</h3>
+											<p>Consequat turpis venenatis varius lorem sed dictum ipsum amet pellentesque fusce hendrerit consectetur risus, sed eget semper nunc.</p>
+										</section>
+										<section>
+											<span class="icon major fa-save style4"></span>
+											<h3>Dolor Consequat</h3>
+											<p>Consequat turpis venenatis varius lorem sed dictum ipsum amet pellentesque fusce hendrerit consectetur risus, sed eget semper nunc.</p>
+										</section>
+									</div>
+								</div>
+							</section>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+						<!-- CTA -->
+							<section id="cta" class="main special">
+								<div class="inner">
+									<p>Sed varius suspendisse dictum leo ipsum amet pellentesque<br />
+									fusce ac hendrerit consectetur tempor lorem ipsum.</p>
+									<ul class="actions vertical">
+										<li><a href="#" class="button special big">Get Started</a></li>
+										<li><a href="#" class="button big">Learn More</a></li>
+									</ul>
+								</div>
+							</section>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+					</div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<ul class="icons">
+								<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon alt fa-youtube"><span class="label">YouTube</span></a></li>
+								<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+							</ul>
+						</div>
+						<p class="copyright">&copy; Untitled. All rights reserved.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+			<script src="{{asset('assets/js/jquery.scrolly.min.js')}}"></script>
+			<script src="{{asset('assets/js/jquery.scrollex.min.js')}}"></script>
+			<script src="{{asset('assets/js/skel.min.js')}}"></script>
+			<script src="{{asset('assets/js/util.js')}}"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="{{asset('assets/js/main.js')}}"></script>
+
+	</body>
 </html>
